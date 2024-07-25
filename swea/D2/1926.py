768b242e9cbf60ff2c3deca43cff49e52b8d82d1
kjm <kjmemail2743@gmail.com>
2024-07-25 23:28:33 +0900

    done 0725

--- swea/D2/1926.py
+++ swea/D2/1926.py
@@ -11,3 +11,15 @@ for i in range(1, N+1):
 
     else:
         print(i, end=' ')
+
+####### 다른 풀이 #######
+N = int(input())
+
+for i in range(1, N+1):
+    i = str(i)
+    cnt = i.count('3') + i.count('6') + i.count('9')
+
+    if cnt == 0:
+        print(i, end=' ')
+    else:
+        print('-'*cnt, end=' ')
