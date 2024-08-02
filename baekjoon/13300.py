e4a579971f82d378cbe082ccb5697eed7b3ada39
kjm <kjmemail2743@gmail.com>
2024-08-02 23:23:11 +0900

    done 24.08.02

--- baekjoon/13300.py
+++ baekjoon/13300.py
@@ -1,21 +1,21 @@
 # 13300. 방 배정
-N, K = map(int, input().split())
-arr = [[0] * 2 for i in range(6)]
+# N, K = map(int, input().split())
+# arr = [[0] * 2 for i in range(6)]
 
-for _ in range(N):
-    s, y = map(int, input().split())
-    arr[y-1][s] += 1
+# for _ in range(N):
+#     s, y = map(int, input().split())
+#     arr[y-1][s] += 1
     
 
-for i in range(len(lst)):
+# for i in range(len(lst)):
 
-    if lst.count(lst[i]) <= 2:
-        cnt += 1
-        print(cnt)
-    else:
-        cnt += int(lst.count(lst[i])/2)
+#     if lst.count(lst[i]) <= 2:
+#         cnt += 1
+#         print(cnt)
+#     else:
+#         cnt += int(lst.count(lst[i])/2)
 
-print(cnt)
+# print(cnt)
 
 
 
