d23caa8866d2b07dd915821ec15ad62c1a06597a
kjm <kjmemail2743@gmail.com>
2024-08-05 00:27:02 +0900

    done 24.08.04

---
+++ swea/D1/9367.py
@@ -0,0 +1,20 @@
+# 9367. 점점 커지는 당근의 개수
+T = int(input())
+for tc in range(1, T+1):
+    N = int(input())
+    arr = list(map(int, input().split()))
+
+    Max = 1
+    cnt = 1
+    for i in range(N-1):
+        if arr[i] < arr[i+1]:
+            cnt += 1
+            if Max < cnt:
+                Max = cnt
+        else:
+            cnt = 1
+
+    print(f'#{tc} {Max}')
+    
+
+
