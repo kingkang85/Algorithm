3f1596313d8a0f2e9345975aec2bc86451ea9e98
kjm <kjmemail2743@gmail.com>
2024-08-09 00:05:03 +0900

    done 24.08.09

---
+++ baekjoon/2564.py
@@ -0,0 +1,27 @@
+# 2564. 경비원
+width, height = map(int, input().split())
+stores = int(input())
+st = []
+for _ in range(stores):
+    st.append(list(map(int, input().split())))  # k 방향, dist 거리
+
+my_k, my_dist = map(int, input().split())  # my_k 동근이의 방향, my_dist 동근이의 거리
+
+result = 0
+for s in st:
+
+    if my_k - s[0] == 0:
+        if my_dist >= s[1]:
+            result += my_dist - s[1]
+        else:
+            result += s[1] - my_dist
+
+    elif my_k - s[0] == 1 or my_k - s[0] == -1:
+        ans1 = my_dist + s[1] + height
+        ans2 = 2 * width - my_dist - s[1] + height
+        result += min(ans1, ans2)
+
+    else:
+        pass
+
+print(result)
