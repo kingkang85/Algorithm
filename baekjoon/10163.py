510e4e07687a95a1cd0951680258a1bff7c5a752
김기산 <chi_927@naver.com>
2024-08-08 18:00:18 +0900

    add 24.08.08

--- baekjoon/10163.py
+++ baekjoon/10163.py
@@ -1,13 +1,35 @@
 # 10163. 색종이
+# N = int(input())
+
+
+# idx_lst = []
+# for i in range(1, N+1):
+#     r1, c1, r2, c2 = map(int, input().split())
+#     idx_lst.append([r1, r1+r2, c1, c1+c2])
+# Max = max(idx_lst)
+# arr = [[0]*]
+
+# for j in range(1, N+1):
+#     cnt = 0
+#     for row in arr:
+#         for col in row:
+#             if col == j:
+#                 cnt += 1
+#     print(cnt)
+
+import sys
+input = sys.stdin.readline
 N = int(input())
 
+arr = [[0] * 1001 for _ in range(1001)]
 
-idx_lst = []
 for i in range(1, N+1):
     r1, c1, r2, c2 = map(int, input().split())
-    idx_lst.append([r1, r1+r2, c1, c1+c2])
-Max = max(idx_lst)
-arr = [[0]*]
+
+    for r in range(r1, r1+r2):
+        for c in range(c1, c1+c2):
+            arr[r][c] = i
+
 
 for j in range(1, N+1):
     cnt = 0
