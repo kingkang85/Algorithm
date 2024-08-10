7bb60287f8753e8051668cb4b0688177440ca5ee
kjm <kjmemail2743@gmail.com>
2024-08-11 02:10:55 +0900

    studied 24.08.10

--- baekjoon/2578.py
+++ baekjoon/2578.py
@@ -1,11 +1,58 @@
 # 2578. 빙고
-def Bingo(my_arr, correct_arr):
-    pass
+def Bingo(check):
+    cnt = 0
+    for i in range(5):
+        for j in range(5):
+            if check[i][j] != 0:
+                break
+        else:
+            cnt += 1
+    for i in range(5):
+        for j in range(5):
+            if check[i][j] != 0:
+                break
+        else:
+            cnt += 1
+    
+    for i in range(5):
+        if check[i][i] != 0:
+            break
+        else:
+            cnt += 1
 
-my_arr = [list(map(int, input().split())) for _ in range(5)]
-correct_arr = [list(map(int, input().split())) for _ in range(5)]
+    for i in range(5):
+        if check[i][4-i] != 0:
+            break
+        else:
+            cnt += 1
+
+    return cnt
+    
+
+                
+             
 
 
+my_bingo = [list(map(int, input().split())) for _ in range(5)]
+correct_bingo = [list(map(int, input().split())) for _ in range(5)]
+
+
+cnt = 0
+for i in range(5):
+    for j in range(5):
+        cnt += 1
+        num = correct_bingo[i][j]
+
+        for i in range(5):
+            for j in range(5):
+                if my_bingo[i][j] == num:
+                    my_bingo[i][j] = 0
+
+        if Bingo(my_bingo) == 3:
+            print(cnt)
+            break
+
+    break
 
 
 
--- baekjoon/2628.py
+++ baekjoon/2628.py
@@ -1,21 +1,34 @@
 # 2628. 종이 자르기
-r, c = map(int, input().split())
-arr = [[0] * c for _ in range(r)]
+w, h = map(int, input().split())
 cut_lines = int(input())
+
+# 가로 세로의 잘린 포인트를 담기 위한 리스트
+# 앞뒤 비교하며 길이를 비교하기 위해 첫과 끝도 추가했음
+row = [0] + [h]
+col = [0] + [w]
 
 for _ in range(cut_lines):
     standard, cut = map(int, input().split())
 
-    # 가로로 자를 때
-    if standard == 0:
-        arr = arr[:cut][:]
-        print(arr)
+    if standard == 0:  # 가로 cutting 포인트
+        row.append(cut)
+    else:  # 세로 cutting 포인트
+        col.append(cut)
 
-    # 세로로 자를 때
-    else:
-        arr = arr[:][:cut]
+row.sort()
+col.sort()
 
-        print(arr)
+# print(row, col)  # [0, 2, 3, 8] [0, 4, 10]
 
+Max = 0
+for i in range(len(row)-1):
+    for j in range(len(col)-1):
+        # 뒤에서 앞 빼면서 넓이 구하기
+        area = (row[i+1] - row[i]) * (col[j+1] - col[j])
 
+        # 최댓값 찾기
+        if Max < area:
+            Max = area
+
+print(Max)
 
