dcf89ede90bf26c057abdf3d895a049b92daa9ef
kjm <kjmemail2743@gmail.com>
2024-08-21 23:18:29 +0900

    studied 24.08.21

--- swea/D3/2805.py
+++ swea/D3/2805.py
@@ -3,6 +3,13 @@ T = int(input())
 for tc in range(1, T+1):
     N = int(input())
     farm = [list(map(int, input())) for _ in range(N)]
-    get = 0
-    print(f'#{tc} {get}')
 
+    get = 0
+    for i in range(N):
+        if i <= N//2:
+            get += sum(farm[i][N//2-i : N//2+i+1])
+
+        else:
+            get += sum(farm[i][N//2-(N-1-i) : N//2+(N-i)])
+        
+    print(f'#{tc} {get}')
