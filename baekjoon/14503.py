0187723e77d9f878cb9994b541a98e55677f1321
kjm <kjmemail2743@gmail.com>
2024-08-28 22:47:22 +0900

    studied 24.08.28

--- baekjoon/14503.py
+++ baekjoon/14503.py
@@ -6,27 +6,33 @@ def bfs(i, j, d):
     dq = deque()
     dq.append([i, j])
     visited[i][j] = 1
-    
+    ri = [-1, 0, 1, 0]
+    ci = [0, 1, 0, -1]
+
     cnt = 1
-    di = [-1, 0, 1, 0]
-    dj = [0, 1, 0, -1]
-    while True:
-        ni = i + di[d]
-        nj = j + dj[d]
+    while dq:
+        ti, tj = dq.popleft()
 
-        if 0 <= ni < N and 0 <= nj < M and room[ni][nj] == 0 and visited[ni][nj] == 0:
-            dq.append([ni, nj])
-            cnt += 1
-            d = (d+3) % 4
-            visited[ni][nj] = 1
-        
-        else:
-            if dq:
-                i, j = dq.popleft()
-                if room[i][j] == 1:
+        for di, dj in [[-1, 0], [0, 1], [1, 0], [0, -1]]:
+            ni, nj = ti + di, tj + dj
+
+            if 0 <= ni < N and 0 <= nj < M and room[ni][nj] == 0 and visited[ni][nj] == 0:
+                d = (d+3) % 4
+                newi, newj = ti + ri[d], tj + ci[d]
+                if visited[newi][newj] == 0:
+                    cnt += 1
+                    dq.append([newi, newj])
+                    visited[newi][newj] = 1
+                    
+                
+            else:
+                newr, newc = ti + ri[d], tj + ci[d]
+                if room[newr][newc] == 1:
                     return cnt
+                else:
+                    dq.append([newr, newc])
+                    visited[newr][newc] = 1
 
-        
 
 N, M = map(int, input().split())
 r, c, d = map(int, input().split())
