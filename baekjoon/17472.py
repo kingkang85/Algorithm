fe3cae824523f59861ae9bd38f25820e49f7550e
강지민 <kjmemail2743@gmail.com>
2024-09-14 18:05:51 +0900

    studied 24.09.14

--- baekjoon/17472.py
+++ baekjoon/17472.py
@@ -1,5 +1,6 @@
 # 17472. 다리 만들기 2
 from collections import deque, defaultdict
+
 # 섬 찾는 함수
 def FindIsland():
     global num
@@ -17,37 +18,36 @@ def FindIsland():
                 island[num].append((i, j))
 
             while q:
-                i, j = q.popleft()
-                for di, dj in [[-1, 0], [0, 1], [1, 0], [0, -1]]:
-                    ni, nj = i + di, j + dj
-                    if 0 <= ni < N and 0 <= nj < M and arr[ni][nj] == 1 and visited[ni][nj] == 0:
-                        arr[ni][nj] = num  # 섬 번호 부여
-                        q.append((ni, nj))
-                        visited[ni][nj] = 1
-                        island[num].append((ni, nj))
+                r, c = q.popleft()  # (주의) i, j로 쓰면 안 된다 ㅠ
+                for dr, dc in [[-1, 0], [0, 1], [1, 0], [0, -1]]:
+                    nr, nc = r + dr, c + dc
+                    if 0 <= nr < N and 0 <= nc < M and arr[nr][nc] == 1 and visited[nr][nc] == 0:
+                        arr[nr][nc] = num  # 섬 번호 부여
+                        q.append((nr, nc))
+                        visited[nr][nc] = 1
+                        island[num].append((nr, nc))
 
 
 # 다리 건설하는 함수
 def Bridge():
+    edges = set()
     for k, v in island.items():
         for i, j in v:
-            r, c = i, j
             for dr, dc in [[-1, 0], [0, 1], [1, 0], [0, -1]]:
-                cnt = 0
-                while True:
-                    nr, nc = r + dr, c + dc
-                    if nr < 0 or nr >= N or nc < 0 or nc >= M or arr[nr][nc] == k:
-                        break
+                nr, nc = i + dr, j + dc
+                length = 0
 
-                    if arr[nr][nc] > k and cnt >= 2:
-                        print(nr, nc)
-                        edges.add((k, arr[nr][nc], cnt))
+                while 0 <= nr < N and 0 <= nc < M and arr[nr][nc] != k:
+                    if arr[nr][nc] != 0:
+                        if length >= 2:
+                            edges.add((k, arr[nr][nc], length))
                         break
-                    cnt += 1
-                    r, c = nr, nc
+                    length += 1
+                    nr, nc = nr + dr, nc + dc
+    
+    return list(edges)
     
     
-
 def Find(x):
     if x != p[x]:
         p[x] = Find(p[x])
@@ -72,17 +72,23 @@ arr = [list(map(int, input().split())) for _ in range(N)]
 
 num = 0  # 섬 개수
 island = defaultdict(list)
-edges = set()
 FindIsland()
-print(arr)
-Bridge()
-print(edges)
+edges = Bridge()
+
+edges.sort(key = lambda x: x[2])
+
 p = [i for i in range(num+1)]
 rank = [0] * (num+1)
 ans = 0
+cnt = 0
 for s, e, w in edges:
     if Find(s) != Find(e):
         Union(s, e)
         ans += w
+        cnt += 1
 
-print(ans)
+# 섬이 모두 연결되었는지 확인 => 간선 수 == 노드 수 - 1
+if cnt == num - 1:
+    print(ans)
+else:
+    print(-1)
