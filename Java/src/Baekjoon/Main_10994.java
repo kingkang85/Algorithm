48f913bf115183f4cdcd65ff0efa0478e9f060ea
강지민 <kjmemail2743@gmail.com>
2024-10-04 00:28:23 +0900

    studied java 24.10.04

---
+++ Java/src/Baekjoon/Main_10994.java
@@ -0,0 +1,12 @@
+package Baekjoon;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main_10994 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        System.out.println(N);
+    }
+}
