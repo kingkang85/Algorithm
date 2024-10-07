6f3a0da28d1747318aedbf6fed6622da8c09c934
강지민 <kjmemail2743@gmail.com>
2024-10-07 22:31:36 +0900

    studied 24.10.07

---
+++ Java/src/Baekjoon/Main_2163.java
@@ -0,0 +1,14 @@
+package Baekjoon;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main_2163 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] input = br.readLine().split(" ");
+        int N = Integer.parseInt(input[0]);
+        int M = Integer.parseInt(input[1]);
+        System.out.println(N * M - 1);
+    }
+}
---
+++ Java/src/Baekjoon/Main_6609.java
@@ -0,0 +1,33 @@
+package Baekjoon;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main_6609 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s;
+        while ((s = br.readLine()) != null) {
+            StringTokenizer st = new StringTokenizer(s);
+            int M = Integer.parseInt(st.nextToken());
+            int P = Integer.parseInt(st.nextToken());
+            int L = Integer.parseInt(st.nextToken());
+            int E = Integer.parseInt(st.nextToken());
+            int R = Integer.parseInt(st.nextToken());
+            int S = Integer.parseInt(st.nextToken());
+            int N = Integer.parseInt(st.nextToken());
+
+            for (int i = 0; i < N; i++) {
+                int newM = P / S;
+                int newP = L / R;
+                int newL = M * E;
+
+                M = newM;
+                P = newP;
+                L = newL;
+            }
+            System.out.println(M);
+        }
+    }
+}
