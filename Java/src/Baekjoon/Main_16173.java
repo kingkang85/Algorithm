c5e64e2e7059252bb1626886f7862badc98fc6f5
강지민 <kjmemail2743@gmail.com>
2024-10-21 02:10:50 +0900

    studied 24.10.21

--- Java/src/Baekjoon/Main_16173.java
+++ Java/src/Baekjoon/Main_16173.java
@@ -6,7 +6,6 @@ import java.util.*;
 public class Main_16173 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         int n = Integer.parseInt(br.readLine());
         int[][] arr = new int[n][n];
         for (int i = 0; i < n; i++) {
@@ -15,13 +14,38 @@ public class Main_16173 {
                 arr[i][j] = Integer.parseInt(input[j]);
             }
         }
-
+        System.out.println(bfs(0, 0, n, arr));
     }
 
-    public static void bfs(int sti, int stj, int n) {
-        Queue<Integer> q = new LinkedList<>();
+    static String bfs(int sti, int stj, int n, int[][] arr) {
+        Queue<int[]> q = new LinkedList<>();
         int[][] visited = new int[n][n];
 
-        q.offer((sti, stj));
+        q.offer(new int[] {sti, stj});
+        visited[sti][stj] = 1;
+
+        int[] di = {0, 1};
+        int[] dj = {1, 0};
+
+        while (!q.isEmpty()) {
+            int[] now = q.poll();
+            int i = now[0];
+            int j = now[1];
+
+            if (i == n-1 && j == n-1) {
+                return "HaruHaru";
+            }
+
+            for (int k = 0; k < 2; k++) {
+                int ni = i + di[k] * arr[i][j];
+                int nj = j + dj[k] * arr[i][j];
+
+                if (ni >= 0 && ni < n && nj >= 0 && nj < n && visited[ni][nj] == 0 && arr[ni][nj] != 0) {
+                    q.offer(new int[] {ni, nj});
+                    visited[ni][nj] = 1;
+                }
+            }
+        }
+        return "Hing";
     }
 }
