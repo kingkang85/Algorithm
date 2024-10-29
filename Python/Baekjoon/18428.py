19e7c84cc579fbbe88cd75ab9393da0f14e772de
강지민 <kjmemail2743@gmail.com>
2024-10-30 00:28:31 +0900

    done 24.10.29

--- Python/Baekjoon/18428.py
+++ Python/Baekjoon/18428.py
@@ -1,16 +1,12 @@
 # 18428. 감시 피하기
-'''
-dfs
-- 장애물 설치 갯수가 3개가 될 때까지 감시를 못 피했다면 NO return
-- 장애물 설치 할 때마다 감시 피했는지 확인 => 피했다면 YES return
-
-'''
 import sys
 input = sys.stdin.readline
 
+# 감시를 피할 수 있는지 확인
 def check():
     for r in range(N):
         for c in range(N):
+            # 선생님의 상하좌우 확인
             if arr[r][c] == 'T':
                 for dr, dc in [[-1, 0], [0, 1], [1, 0], [0, -1]]:
                     nr, nc = r + dr, c + dc
@@ -18,27 +14,30 @@ def check():
                         if arr[nr][nc] == 'O':
                             break
                         elif arr[nr][nc] == 'S':
-                            return False
-                        nr += drS
+                            return False  # 학생 발각
+                        nr += dr
                         nc += dc
-    
     return True
 
 def dfs(cnt):
-    if cnt == 3 and check():
-        return 'Yes'
+    # 장애물 3개 설치 후 감시 체크
+    if cnt == 3:
+        return check()
 
     for i in range(N):
         for j in range(N):
             if arr[i][j] == 'X':
                 arr[i][j] = 'O'
-                dfs(cnt + 1)
+                if dfs(cnt + 1):
+                    return True
                 arr[i][j] = 'X'
+                
+    return False
 
-    return 'NO'
-    
 N = int(input())
 arr = [input().split() for _ in range(N)]
 
-dfs(0)
-print('No')
+if dfs(0):
+    print('YES')
+else:
+    print('NO')
