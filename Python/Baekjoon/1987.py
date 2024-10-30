a1cdbaefd2f7b6e3f6d77eb2eaa42dfb42a1f37c
강지민 <kjmemail2743@gmail.com>
2024-10-31 01:20:44 +0900

    studied 24.10.30

--- Python/Baekjoon/1987.py
+++ Python/Baekjoon/1987.py
@@ -8,21 +8,19 @@ def dfs(i, j, cnt):
     if arr[i][j] in used:
         maxV = max(maxV, cnt)
         return
-
+    
     used.add(arr[i][j])
+    
     for di, dj in [[-1, 0], [0, 1], [1, 0], [0, -1]]:
         ni, nj = i + di, j + dj
-        if 0 <= ni < R and 0 <= nj < C and visited[ni][nj] == 0:
-            visited[ni][nj] = 1
-            used.add(arr[ni][nj])
+        if 0 <= ni < R and 0 <= nj < C:
             dfs(ni, nj, cnt + 1)
-            visited[ni][nj] = 0
+            used.remove(arr[ni][nj])
 
 R, C = map(int, input().split())
 arr = [input().strip() for _ in range(R)]
 
 maxV = 0
-visited = [[0] * C for _ in range(R)]
 used = set()
 dfs(0, 0, 1)
 print(maxV)
