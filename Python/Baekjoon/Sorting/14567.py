c57f197076103afa3f90f3683a849af7e3b7aba8
강지민 <kjmemail2743@gmail.com>
2024-11-11 22:38:44 +0900

    review 24.11.11

--- Python/Baekjoon/Sorting/14567.py
+++ Python/Baekjoon/Sorting/14567.py
@@ -1,38 +1,35 @@
 # 14567. 선수과목
-import sys
 from collections import deque
+import sys
 input = sys.stdin.readline
 
 def topology_sort():
     q = deque()
-    result = [1] * (N+1)  # 최소 1학기로 초기화
+    result = [0] * (N+1)  # 각 과목별 최소 학기
 
     for i in range(1, N+1):
         if indegree[i] == 0:
-            q.append(i)
-    
+            q.append((i, 1))
+        
     while q:
-        now = q.popleft()
+        now, time = q.popleft()
+        result[now] = time
+
         for next in graph[now]:
             indegree[next] -= 1
-
-            # 현재 학기 + 1과 다음 학기 중 최댓값으로 갱신
-            result[next] = max(result[next], result[now] + 1)
-
             if indegree[next] == 0:
-                q.append(next)
+                q.append((next, time+1))
     
-    return result[1:]
-    
-    
+    return result
+
 N, M = map(int, input().split())
-
-indegree = [0] * (N+1)
 graph = [[] for _ in range(N+1)]
-for _ in range(M):
-    A, B = map(int, input().split())
-    graph[A].append(B)
-    indegree[B] += 1
+indegree = [0] * (N+1)  # 진입 차수
 
-result = topology_sort()
-print(*result)
+# 선행 관계 파악
+for _ in range(M):
+    a, b = map(int, input().split())
+    graph[a].append(b)
+    indegree[b] += 1
+
+print(*topology_sort()[1:])
