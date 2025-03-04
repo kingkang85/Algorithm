4a9c50b875d1a316c108dbffdf1f4b6dbfe92d70
강지민 <kjmemail2743@gmail.com>
2025-03-04 18:00:11 +0900

    study bfs 25.03.04

---
+++ Python/Baekjoon/2565rr.py
@@ -0,0 +1,6 @@
+# 2565. 전깃줄
+# 11:54 ~
+import sys
+input = sys.stdin.readline
+
+N = int(input())
---
+++ Python/Baekjoon/BFS/16236rr.py
@@ -0,0 +1,6 @@
+# 16236. 아기 상어
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+arr = [list(map(int, input().split())) for _ in range(N)]
