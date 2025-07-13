557028f7d71b37a83305fd17629a9c6dbc719e4a
강지민 <kjmemail2743@gmail.com>
2025-07-13 22:48:34 +0900

    Java Permutation 25.07.13

---
+++ Java/src/Baekjoon/Backtracking/Main_15651.java
@@ -0,0 +1,38 @@
+package Baekjoon.Backtracking;
+
+import java.io.*;
+import java.util.*;
+
+public class Main_15651 {
+    static int N, M;
+    static int[] result;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        result = new int[M];
+        permutation(0);
+
+        System.out.println(sb);
+    }
+
+    static void permutation(int depth) {
+        if (depth == M) {
+            for (int i = 0; i < M; i++) {
+                sb.append(result[i]).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for (int i = 1; i <= N; i++) {
+            result[depth] = i;
+            permutation(depth + 1);
+        }
+    }
+}
