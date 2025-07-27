e3eadc0698f680eb6e7cc5936bc2890cc67c22d2
강지민 <kjmemail2743@gmail.com>
2025-07-27 22:48:23 +0900

    Java Math 25.07.27

---
+++ Java/src/Baekjoon/Math/Main_10869.java
@@ -0,0 +1,20 @@
+package Baekjoon.Math;
+
+import java.io.*;
+import java.util.*;
+
+public class Main_10869 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+
+        System.out.println(A+B);
+        System.out.println(A-B);
+        System.out.println(A*B);
+        System.out.println(A/B);
+        System.out.println(A%B);
+    }
+}
