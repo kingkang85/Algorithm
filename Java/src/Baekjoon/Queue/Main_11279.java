f7b982749bb6f00d5c70bd2fe3ebfe93202c6106
강지민 <kjmemail2743@gmail.com>
2025-07-29 10:45:13 +0900

    Java Max Heap 25.07.29

---
+++ Java/src/Baekjoon/Queue/Main_11279.java
@@ -0,0 +1,26 @@
+package Baekjoon.Queue;
+
+import java.io.*;
+import java.util.*;
+
+public class Main_11279 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+
+        for (int i = 0; i < N; i++) {
+            int x = Integer.parseInt(br.readLine());
+
+            if (x != 0) {
+                pq.offer(x);
+            } else {
+                sb.append((!pq.isEmpty()) ? pq.poll() : 0).append('\n');
+            }
+        }
+
+        System.out.println(sb);
+    }
+}
