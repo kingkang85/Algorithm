b6329b9801121176a64258fdc4c937978dc426b6
강지민 <kjmemail2743@gmail.com>
2025-08-01 07:52:24 +0900

    Java Math 25.08.01

---
+++ Java/src/Baekjoon/Math/Main_2558.java
@@ -0,0 +1,13 @@
+package Baekjoon.Math;
+
+import java.io.*;
+
+public class Main_2558 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int A = Integer.parseInt(br.readLine());
+        int B = Integer.parseInt(br.readLine());
+
+        System.out.println(A+B);
+    }
+}
