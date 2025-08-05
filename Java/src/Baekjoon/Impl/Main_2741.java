2b399efa30ec99ddeb2b4ad5e978505928f8161b
강지민 <kjmemail2743@gmail.com>
2025-08-05 20:41:30 +0900

    Java Impl 25.08.05

---
+++ Java/src/Baekjoon/Impl/Main_2741.java
@@ -0,0 +1,16 @@
+package Baekjoon.Impl;
+
+import java.io.*;
+
+public class Main_2741 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+        for (int i = 1; i <= N; i++) {
+            sb.append(i).append('\n');
+        }
+        System.out.println(sb);
+    }
+}
