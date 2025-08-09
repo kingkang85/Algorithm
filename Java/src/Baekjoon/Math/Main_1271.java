ea964eca5f5d54229cbc017dcb0420c40595fce7
강지민 <kjmemail2743@gmail.com>
2025-08-10 01:12:11 +0900

    Java Math 25.08.10

---
+++ Java/src/Baekjoon/Math/Main_1271.java
@@ -0,0 +1,18 @@
+package Baekjoon.Math;
+
+import java.io.*;
+import java.math.BigInteger;
+import java.util.*;
+
+public class Main_1271 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        BigInteger n = new BigInteger(st.nextToken());
+        BigInteger m = new BigInteger(st.nextToken());
+
+        System.out.println(n.divide(m));
+        System.out.println(n.mod(m));
+    }
+}
